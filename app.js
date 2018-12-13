31f16fecee34b7072f8f03ca9387b3a036d7d3ca
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2018-12-13 14:17:18 -0700

    putting things together

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 class Game {
   constructor(config) {
     this.mainCharacter = config.char;
+    this.creationDate = config.creationDate;
   }
 }
 
 class Character {
   constructor(config) {
     this.charName = config.charName || 'Fergoddanaim';
-    this.str = config.str || 1;
-    this.agi = config.agi || 1;
-    this.con = config.con || 1;
-    this.int = config.int || 1;
-    this.wil = config.wil || 1;
+    this.str = config.str || 5;
+    this.agi = config.agi || 5;
+    this.con = config.con || 5;
+    this.int = config.int || 5;
+    this.wil = config.wil || 5;
   }
 }
 
@@ -23,11 +24,74 @@ class PC extends Character {
 
 let gameSaves = [];
 
+function createForm(a) {
+  let final = document.createElement('form');
+  final.setAttribute('id', a);
+  return final;
+}
+
+function placeForm(a, b, c) {
+  let parent = document.getElementById(a);
+  parent.append(b);
+  return document.getElementById(c);
+}
+
+function createField(fieldName, fieldType, area) {
+  let final = document.createElement('input');
+  final.setAttribute('class', fieldName);
+  final.setAttribute('name', area);
+  final.setAttribute('value', fieldName);
+  final.setAttribute('type', fieldType);
+  return final;
+}
+
+function createText(text) {
+  let final = document.createElement('p');
+  let textSection = document.createTextNode(text);
+  final.appendChild(textSection);
+  return final;
+}
+
 function createNewGame() {
-  let a = {
-    creationDate: 0
+  let a = {};
+  function createPlayerCharacter() {
+    let final = {};
+
+    let z = 'creation-form';
+    let form = placeForm('creation-space', createForm(z), z);
+
+    // final.charName = prompt('What is your name?');
+
+    function selectGoodStat() {
+      let lineBreak = document.createElement('br');
+      let strField = createField('str', 'radio', 'stat');
+      let strText = createText('Strength');
+      form.append(strText);
+      form.append(strField);
+      let agiField = createField('agi', 'radio', 'stat');
+      let agiText = createText('Agility');
+      form.append(agiText);
+      form.append(agiField);
+      let conField = createField('con', 'radio', 'stat');
+      let conText = createText('Constitution');
+      form.append(conText);
+      form.append(conField);
+      let intField = createField('int', 'radio', 'stat');
+      let intText = createText('Intelligence');
+      form.append(intText);
+      form.append(intField);
+      let wilField = createField('wil', 'radio', 'stat');
+      let wilText = createText('Will');
+      form.append(wilText);
+      form.append(wilField);
+    }
+    selectGoodStat();
+    
+    return final;
   }
+  let b = createPlayerCharacter();
   a.char = new PC(b);
-  a.creationDate = Math.floor(Date.now() / 1000)
-  gameSaves.push(new Game(a))
+  a.creationDate = Math.floor(Date.now() / 1000);
+  gameSaves.push(new Game(a));
+  console.log(gameSaves);
 }
